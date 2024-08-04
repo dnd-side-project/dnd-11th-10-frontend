46d097d5fb954c7d87b6a780bfb48be782a61068
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-04 23:52:13 +0900

    feat: 사용할 color 상수화

---
+++ src/styles/theme/colors.ts
@@ -0,0 +1,36 @@
+export const colors = {
+  primary: {
+    50: '#BBFFE9',
+    100: '#AAF4E0',
+    200: '#75F0D0',
+    300: '#35E2B4',
+    400: '#47D3AD',
+  },
+  secondary: {
+    50: '#E5DCF7',
+    100: '#CEBDF0',
+    200: '#A683E9',
+    300: '#8C5EE4',
+    400: '#6D37D3',
+  },
+  designPurple: {
+    50: '#F4D6FF',
+    100: '#E9C0F9',
+    200: '#D283F0',
+    300: '#C55EEC',
+    400: '#B230E3',
+  },
+  devBlue: {
+    50: '#DDECFF',
+    100: '#B8D3F4',
+    200: '#88B5ED',
+    300: '#629FEC',
+    400: '#2F7CDE',
+  },
+  gradient: {
+    1: 'linear-gradient(0.25turn, #00FFBB, #3D7DF3, #6E32E6);',
+  },
+  background: '#0E121B',
+  error: '#D13838',
+  errorLight: '#EF5E5E'
+}

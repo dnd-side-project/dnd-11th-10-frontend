ad74283cdd9b15fab123d088befd1819fcf7264b
lsy20140 <lsy20140@naver.com>
2024-08-04 19:45:03 +0900

    feat: root 레이아웃 구성 및 Pretendard 폰트 적용

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from 'next'
 import './globals.css'
+import localFont from 'next/font/local'
+
+const pretendard = localFont({
+  src: '../../public/fonts/PretendardVariable.woff2',
+  display: 'swap',
+  weight: '45 920',
+  variable: '--font-pretendard',
+});
 
 export const metadata: Metadata = {
   title: 'SPACE D',
@@ -13,7 +21,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
-    </html>
+      <body className={`flex justify-center min-h-dvh ${pretendard.variable} font-pretendard`}>
+        <div className="w-full max-w-[430px] bg-black">
+          {children}
+        </div>
+      </body>
+    </html >
   )
 }

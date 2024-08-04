d396b643554b8926e1a79d54e136efd8155e9f93
lsy20140 <lsy20140@naver.com>
2024-08-05 02:15:35 +0900

    feat: 메인 페이지 Header 컴포넌트 생성

---
+++ src/app/home/layout.tsx
@@ -0,0 +1,11 @@
+import Header from '../components/domain/home/Header'
+import Tabs from '../components/domain/home/Tabs'
+
+export default function HomeLayout() {
+  return (
+    <>
+      <Header />
+      <Tabs />
+    </>
+  )
+}

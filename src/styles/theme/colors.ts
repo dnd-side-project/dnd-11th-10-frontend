6304bfc7b5b5adff6c9922d6ba9db9e48a2589dc
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-05 14:23:22 +0900

    feat: 의논된 색상 등록 및 opacity 컬러 추가

--- src/styles/theme/colors.ts
+++ src/styles/theme/colors.ts
@@ -13,6 +13,15 @@ export const colors = {
     300: '#8C5EE4',
     400: '#6D37D3',
   },
+  gray: {
+    200: '#DCE3EB',
+    300: '#B4C0CD',
+    400: '#9CAAB9',
+    500: '#637180',
+    600: '#3C4754',
+    700: '#2F363E',
+    800: '#212830',
+  },
   designPurple: {
     50: '#F4D6FF',
     100: '#E9C0F9',
@@ -28,9 +37,18 @@ export const colors = {
     400: '#2F7CDE',
   },
   gradient: {
-    1: 'linear-gradient(0.25turn, #00FFBB, #3D7DF3, #6E32E6);',
+    100: 'linear-gradient(0.25turn, #00FFBB, #3D7DF3, #6E32E6);',
+    200: 'linear-gradient(0.25turn, #1FD5BC, #358BF2);',
+    300: 'linear-gradient(0.25turn, #1A90E8, #5143E3);',
+    400: 'linear-gradient(0.25turn, #C439F1, #6D32E6);',
   },
   background: '#0E121B',
   error: '#D13838',
-  errorLight: '#EF5E5E'
+  errorLight: '#EF5E5E',
+  outline: 'rgb(255,255,255,0.12)',
+  onSurface: {
+    100: 'rgb(255,255,255,0.38)',
+    200: 'rgb(255,255,255,0.6)',
+    300: 'rgb(255,255,255,0.87)',
+  }
 }

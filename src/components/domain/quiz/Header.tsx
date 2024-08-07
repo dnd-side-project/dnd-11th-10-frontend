6f6d5c1acdac49df7329a7781b0461a97b5b1bc4
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-07 15:10:12 +0900

    feat: quiz의 header 추가

--- src/components/domain/quiz/Header.tsx
+++ src/components/domain/quiz/Header.tsx
@@ -1,7 +1,49 @@
+'use client'
+
 import React from 'react'
+import Image from 'next/image'
+
+import Cross from '/public/icons/cross.svg'
+import Back from '/public/icons/back.svg'
 
 function Header() {
-  return <div className='text-gray-100'>Header</div>
+  let currentProblems = 1
+  const problems = 5
+  const isBack = true
+
+  return (
+    <div className="flex justify-around p-4 h-20 ">
+      {isBack ? (
+        <div className="mt-3">
+          <Image
+            src={Back}
+            alt="Back.svg"
+            onClick={() => console.log('back logo clicked')}
+            className="cursor-pointer"
+          />
+        </div>
+      ) : (
+        <div className="mt-3">
+          <Image
+            src={Cross}
+            alt="cross.svg"
+            onClick={() => console.log('cross logo clicked')}
+            className="cursor-pointer"
+          />
+        </div>
+      )}
+
+      <div className="mt-4 w-72 h-3 bg-outline rounded-2xl" />
+
+      <div className="flex mt-2 text-onSurface-300 text-xl">
+        {currentProblems}
+        <p className="text-onSurface-200">
+          {'/'}
+          {problems}
+        </p>
+      </div>
+    </div>
+  )
 }
 
 export default Header

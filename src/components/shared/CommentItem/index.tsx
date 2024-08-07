92d04c8df326204d843bb8d43a10d2b96b6c37ca
lsy20140 <lsy20140@naver.com>
2024-08-08 02:41:01 +0900

    feat: 댓글 컴포넌트(CommentItem) UI 작업

---
+++ src/components/shared/CommentItem/index.tsx
@@ -0,0 +1,80 @@
+import CategoryTag from '@/components/shared/CategoryTag'
+import WordTag from '@/components/shared/WordTag'
+import { SimpleCommentType } from '@/types/comment'
+import { getTimeAgo } from '@/utils/date'
+import Image from 'next/image'
+import Link from 'next/link'
+
+interface CommentItemProps extends SimpleCommentType {
+  showTags: boolean
+}
+
+export default function CommentItem({
+  showTags,
+  id,
+  content,
+  likeCount,
+  createdAt,
+  writerInfo: {
+    id: writerId,
+    nickname,
+    profileImage,
+    company,
+    jobGroup,
+    experience,
+  },
+  wordInfo: { id: wordId, name, pronunciationInfo, category },
+}: CommentItemProps) {
+  return (
+    <Link
+      href={`/word/${wordId}`}
+      className="flex flex-col gap-3 justify-between p-5 bg-gray-800 rounded-2xl"
+    >
+      <div className="flex justify-between">
+        <div className="flex gap-2">
+          <Image
+            src={'/images/logo.svg'}
+            width={38}
+            height={38}
+            alt="profile"
+          />
+          <div>
+            <p className="text-onSurface-300 text-sm">{nickname}</p>
+            <p className="text-onSurface-200 text-xs">
+              {jobGroup + ' · ' + company + ' · ' + experience}
+            </p>
+          </div>
+        </div>
+        <Image
+          alt="menu"
+          src={'/icons/vertical_menu.svg'}
+          width={24}
+          height={24}
+        />
+      </div>
+      <p className="text-onSurface-300 break-keep overflow-hidden line-clamp-3">
+        {content}
+      </p>
+      {/* 메인 '반응이 뜨거운 댓글' 파트에서만 태그 표기 */}
+      {showTags && (
+        <div className="flex gap-2">
+          <WordTag text={name} />
+          <WordTag text={pronunciationInfo.korean} />
+          <CategoryTag category={category} />
+        </div>
+      )}
+      <div className="flex justify-between">
+        <div className="flex gap-1">
+          <Image
+            alt="좋아요"
+            src={'/icons/heart_outline.svg'}
+            width={16}
+            height={16}
+          />
+          <p className="text-xs text-onSurface-200">{likeCount}</p>
+        </div>
+        <p className="text-xs text-onSurface-200">{getTimeAgo(createdAt)}</p>
+      </div>
+    </Link>
+  )
+}

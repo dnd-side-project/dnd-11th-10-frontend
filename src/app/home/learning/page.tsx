8bd736b2febf9c5a45c0d200132acd6db61c820f
lsy20140 <lsy20140@naver.com>
2024-08-10 00:46:41 +0900

    design: 홈(메인) 화면 레이아웃 적용 및 screen xs 값 추가

--- src/app/home/learning/page.tsx
+++ src/app/home/learning/page.tsx
@@ -1,3 +1,14 @@
+import { WordsList } from '@/components/domain/home/dictionary/RecentlyAddedWords/data'
+import CommunicationStats from '@/components/domain/home/learning/CommunicationStats'
+import TodayQuiz from '@/components/domain/home/learning/TodayQuiz'
+import TodayWords from '@/components/domain/home/learning/TodayWords'
+
 export default function LearningTab() {
-  return <p className="text-white">LearningTab</p>
+  return (
+    <div className="w-full my-12 flex flex-col items-center gap-[60px]">
+      <TodayQuiz todaySolvedCnt={50} />
+      <TodayWords wordsList={WordsList} />
+      <CommunicationStats />
+    </div>
+  )
 }

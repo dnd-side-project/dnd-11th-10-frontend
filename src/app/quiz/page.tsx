db49710241fc6c5565b1ec0e4c9d1a62ae9f48cc
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-09 17:05:55 +0900

    refactor: 시작 레이아웃 분리

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -1,14 +1,12 @@
+'use client'
+
+import Start from '@/components/domain/quiz/Start'
 import React from 'react'
 
 function Quiz() {
   return (
     <div className="mt-10">
-      <div className="text-center text-onSurface-300 text-xl">
-        <p>업무 소통 능력을 상승 시켜줄</p>
-        <p>실무 용어 퀴즈를 진행하러 가볼까요?</p>
-      </div>
-      <div className="mt-10 h-80 bg-gray-800" />
-      <button className="m-5 px-32 bg-primary-200">시작하기</button>
+      <Start />
     </div>
   )
 }
---
+++ src/components/domain/quiz/Start.tsx
@@ -0,0 +1,16 @@
+const Start = () => {
+  return (
+    <div>
+      <div className="text-center text-onSurface-300 text-xl">
+        <p>업무 소통 능력을 상승 시켜줄</p>
+        <p>실무 용어 퀴즈를 진행하러 가볼까요?</p>
+      </div>
+      <div className="mt-10 h-80 bg-gray-800" />
+      <button className="mt-5 px-6 w-full h-14 bg-gradient-to-tr from-gradient-201 to-gradient-202 to-70% rounded-md text-onSurface-300">
+        시작하기
+      </button>
+    </div>
+  )
+}
+
+export default Start

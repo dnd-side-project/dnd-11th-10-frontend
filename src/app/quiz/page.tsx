eefac7b022707b9fe72c2020b32907acd72392a3
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-09 17:55:44 +0900

    chore: 동적 컬러 적용 각 component에 따라 다른 색상 적용

--- src/app/quiz/page.tsx
+++ src/app/quiz/page.tsx
@@ -13,22 +13,22 @@ const quizType: QuizType[] = [
   {
     participantCount: 10,
     name: '전체 실무',
-    color: 'secondary-200',
+    color: 'text-secondary-200',
   },
   {
     participantCount: 10,
     name: '개발',
-    color: 'devBlue-200',
+    color: 'text-devBlue-200',
   },
   {
     participantCount: 10,
     name: '디자인',
-    color: 'designPurple-200',
+    color: 'text-designPurple-200',
   },
   {
     participantCount: 10,
     name: '비즈니스',
-    color: 'primary-400',
+    color: 'text-primary-400',
   },
 ]
 
@@ -47,11 +47,10 @@ function Quiz() {
               key={quiz.name}
             >
               <div className="">
-                오늘{' '}
-                <p className={`text-${quiz.color}`}>{quiz.participantCount}</p>
+                오늘 <p className={`${quiz.color}`}>{quiz.participantCount}</p>
                 명이 참여 중
               </div>
-              <p className={`text-${quiz.color}`}>{quiz.name}</p>
+              <p className={`${quiz.color}`}>{quiz.name}</p>
               <p>용어</p>
               <p>퀴즈하기</p>
               <div className="" />

7be1f639ef716c37df123eb44b3611f28526598a
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-11 17:26:42 +0900

    refactor:  퀴즈타입 선택 레이아웃 분리

--- src/components/domain/quiz/Start.tsx
+++ src/components/domain/quiz/Start.tsx
@@ -6,7 +6,7 @@ const Start = () => {
         <p>실무 용어 퀴즈를 진행하러 가볼까요?</p>
       </div>
       <div className="mt-10 h-80 bg-gray-800" />
-      <button className="mt-5 px-6 w-full h-14 bg-gradient-to-tr from-gradient-201 to-gradient-202 to-70% rounded-md text-onSurface-300">
+      <button className="mt-52 px-6 w-full h-14 bg-gradient-to-tr from-gradient-201 to-gradient-202 to-70% rounded-md text-onSurface-300">
         시작하기
       </button>
     </div>

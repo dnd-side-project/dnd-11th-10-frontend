1dfdbede795f1871e22a6c0d46ca464b7cfadc5b
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-15 14:28:30 +0900

    chore: 경로 수정

--- src/app/quiz/start/layout.tsx
+++ src/app/quiz/start/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Header from '@/components/domain/quiz/quizstart/Header'
+import Header from '@/components/domain/quiz/start/Header'
 
 type LayoutProps = {
   children: React.ReactNode

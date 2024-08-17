9c35f68743d7226c7f140b3b5a4c6f972d743959
lsy20140 <lsy20140@naver.com>
2024-08-18 01:10:15 +0900

    feat: 용어 상세 페이지 문의하러 가기 버튼 컴포넌트 UI 작업

---
+++ src/components/domain/wordDetail/ContactButton/index.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image'
+import Link from 'next/link'
+
+export default function ContactButton() {
+  return (
+    <Link
+      href={'#'}
+      className="flex justify-between gap-6 bg-gray-800 rounded-2xl py-5 px-7 mx-4"
+    >
+      <Image alt="image" src={''} width={50} height={50} />
+      <div>
+        <p className="text-onSurface-200">실무 용어 관련 문의가 있으신가요?</p>
+        <p className="text-sub1">문의하러 가기</p>
+      </div>
+      <Image
+        alt="contact"
+        src={'/icons/right_arrow.svg'}
+        width={24}
+        height={24}
+      />
+    </Link>
+  )
+}

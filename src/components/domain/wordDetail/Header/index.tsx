559d6212014723e7bf044c9bd4f04d837d44b866
lsy20140 <lsy20140@naver.com>
2024-08-19 16:20:07 +0900

    feat: 용어 목록, 상세페이지 router 설정

--- src/components/domain/wordDetail/Header/index.tsx
+++ src/components/domain/wordDetail/Header/index.tsx
@@ -1,10 +1,13 @@
+'use client'
 import Image from 'next/image'
+import { useRouter } from 'next/navigation'
 
 type HeaderProps = {
   markedCount: number
 }
 
 export default function Header({ markedCount }: HeaderProps) {
+  const router = useRouter()
   return (
     <>
       <header className="w-full h-[90px] flex justify-between items-center px-4 border-b-[1px] border-outline">
@@ -13,6 +16,8 @@ export default function Header({ markedCount }: HeaderProps) {
           src={'/icons/left_arrow.svg'}
           width={24}
           height={24}
+          onClick={() => router.back()}
+          className="cursor-pointer"
         />
         <p className="text-sub1">실무 영어</p>
         <div className="text-center">

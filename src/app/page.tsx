dd385d8afcefd3f194c0054d7d0870e92322d5ee
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-21 01:19:19 +0900

    feat: 비회원 버튼 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,14 @@
+import Button from '@/components/common/Button'
 import Image from 'next/image'
 import { redirect } from 'next/navigation'
 
 export default function Home() {
-  return <div className="text-primary-50">스플래시 화면</div>
+  return (
+    <div className="text-primary-50">
+      
+      <Button type={'black'} isFullWidth={true}>
+        비회원으로 시작하기
+      </Button>
+    </div>
+  )
 }

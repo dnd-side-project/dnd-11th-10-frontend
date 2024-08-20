e9c00bc1521f9e4a11b6ed873a71730a6e605f0b
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-20 11:27:26 +0900

    design: 분야 선택 페이지 레이아웃 변경 적용

--- src/app/quiz/typechoice/page.tsx
+++ src/app/quiz/typechoice/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import Image from 'next/image'
 import { useRouter } from 'next/navigation'
 import React, { useState } from 'react'
 
@@ -47,29 +48,43 @@ function QuizTypeChoice() {
       </div>
 
       <div className="mt-5 flex flex-wrap justify-between">
-        {quizType.map((quiz) => {
+        {quizType.map((quiz, index) => {
           const isSelected = selectedQuiz === quiz.name
           return (
             <div
-              className={`mt-4 p-4 w-[190px] h-[248px] bg-gray-800 text-onSurface-200 rounded-xl cursor-pointer ${
-                isSelected ? 'bg-gray-700 border-[1px] border-primary-400' : ''
+              className={`flex justify-between mt-4 p-4 w-[396px] h-[95px] bg-gray-800 text-onSurface-200 rounded-xl cursor-pointer ${
+                isSelected
+                  ? 'bg-primary-0 border-[1.5px] border-primary-400'
+                  : ''
               }`}
               key={quiz.name}
               onClick={() => handleClick(quiz.name)}
             >
-              <div className="flex">
-                오늘&nbsp;
-                <p className={`${quiz.color}`}>{quiz.participantCount}</p>
-                명이 참여 중
-              </div>
-              <div className="mt-2 flex text-xl">
-                <p className={`${quiz.color}`}>{quiz.name}</p>
-                <p>용어</p>
-              </div>
-              <p className="text-xl">퀴즈하기</p>
-              <div
-                className={`mt-2 w-[158px] h-[120px] ${isSelected ? 'bg-gray-600' : 'bg-gray-700'}`}
+              <Image
+                alt={`type_${index + 1}`}
+                src={`/icons/type_0${index + 1}.svg`}
+                width={63}
+                height={63}
               />
+              <div className='mr-24'>
+                <div className="mt-2 flex text-xl">
+                  <p className={`${quiz.color}`}>{quiz.name}&nbsp;</p>
+                  <p>용어</p>
+                </div>
+                <div className="flex">
+                  오늘&nbsp;
+                  <p className={`${quiz.color}`}>{quiz.participantCount}</p>
+                  명이 참여 중
+                </div>
+              </div>
+              <div className="py-4">
+                <Image
+                  alt="다음"
+                  src={'/icons/front.svg'}
+                  width={24}
+                  height={24}
+                />
+              </div>
             </div>
           )
         })}
@@ -82,7 +97,7 @@ function QuizTypeChoice() {
           disabled={!selectedQuiz}
           className={`mt-5 px-6 w-[270px] h-14 rounded-md ${
             selectedQuiz
-              ? 'bg-gradient-to-br from-gradient-201 to-gradient-202 to-70% text-onSurface-100'
+              ? 'bg-gradient-to-r from-gradient-201 to-gradient-202 to-95% text-background'
               : 'bg-gray-800 text-onSurface-100'
           }`}
           onClick={() => router.push(`/quiz/problem/${selectedQuiz}/1`)}

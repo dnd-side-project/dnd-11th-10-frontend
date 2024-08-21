ff9f1d4478724a87632ab2cefda3527eaee7983e
lsy20140 <lsy20140@naver.com>
2024-08-22 07:54:32 +0900

    feat: 특정 단어에 색상 적용하는 HighlightText 공통 컴포넌트 구현

---
+++ src/components/common/HighlightText/index.tsx
@@ -0,0 +1,28 @@
+export type HighlightTextProps = {
+  text: string
+  target: string
+}
+
+export default function HighlightText({ text, target }: HighlightTextProps) {
+  const escapeRegExp = (str: string) => {
+    return str.replace(/[()]/g, '\\$&') // 특수 문자 이스케이프 처리
+  }
+
+  const regex = new RegExp(`(${escapeRegExp(target)})`, 'g') // target 기준으로 split하지만 ()에 포함시켜 target 포함하여 배열 반환
+
+  const splitted = text.split(regex)
+
+  return (
+    <p className="text-sub2 text-background">
+      {splitted.map((item, idx) =>
+        item === target ? (
+          <span key={idx} className="text-[#00946D]">
+            {item}
+          </span>
+        ) : (
+          item
+        ),
+      )}
+    </p>
+  )
+}
--- src/components/domain/wordDetail/WordInfo/index.tsx
+++ src/components/domain/wordDetail/WordInfo/index.tsx
@@ -1,3 +1,4 @@
+import HighlightText from '@/components/common/HighlightText'
 import HorizontalScrollArea from '@/components/common/HorizontalScrollArea'
 import CategoryTag from '@/components/shared/CategoryTag'
 import { DetailWordType } from '@/types/word'
@@ -46,7 +47,7 @@ export default function WordInfo({ word }: { word: DetailWordType }) {
           {example.map((example, idx) => (
             <div key={idx} className="min-w-fit">
               <div className="max-w-[calc(100%-9px)] relative bg-gray-200 h-fit py-6 px-5 rounded-2xl">
-                <p className="text-sub2 text-background break-keep">{`"${example.text}"`}</p>
+                <HighlightText text={`"${example.text}"`} target={name} />
                 <Image
                   alt="bubble"
                   src={'/images/bubble_tail.svg'}

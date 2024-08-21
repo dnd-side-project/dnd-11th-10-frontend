2e4692b0a4a922735f5bd62c0c15461b0c47926b
lsy20140 <lsy20140@naver.com>
2024-08-22 07:57:50 +0900

    fix: 대소문자 구분하지 않고 target 찾도록 수정(gi 플래그로 변경 및 toLocaleLowerCase() 적용)

--- src/components/common/HighlightText/index.tsx
+++ src/components/common/HighlightText/index.tsx
@@ -8,14 +8,14 @@ export default function HighlightText({ text, target }: HighlightTextProps) {
     return str.replace(/[()]/g, '\\$&') // 특수 문자 이스케이프 처리
   }
 
-  const regex = new RegExp(`(${escapeRegExp(target)})`, 'g') // target 기준으로 split하지만 ()에 포함시켜 target 포함하여 배열 반환
+  const regex = new RegExp(`(${escapeRegExp(target)})`, 'gi') // target 기준으로 split하지만 ()에 포함시켜 target 포함하여 배열 반환
 
   const splitted = text.split(regex)
 
   return (
     <p className="text-sub2 text-background">
       {splitted.map((item, idx) =>
-        item === target ? (
+        item.toLocaleLowerCase() === target.toLocaleLowerCase() ? (
           <span key={idx} className="text-[#00946D]">
             {item}
           </span>

cc84216aa2d165a7d6428b666cab6f7970809d8b
lsy20140 <lsy20140@naver.com>
2024-08-22 08:07:14 +0900

    design: HighlightText 공통 컴포넌트 스토리 작성

---
+++ src/components/common/HighlightText/HighlightText.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import HighlightText from '.'
+
+const meta: Meta<typeof HighlightText> = {
+  title: 'components/common/HighlightText',
+  component: HighlightText,
+  tags: ['autodocs'],
+  argTypes: {
+    text: {
+      description: 'target 단어를 포함한 `전체 문장 또는 단어`입니다.',
+    },
+    target: {
+      description:
+        '`#00946D`으로 highlight 하고싶은 `특정 단어`입니다.<br/>(대소문자 구분X)',
+    },
+  },
+}
+
+export default meta
+
+type Story = StoryObj<typeof HighlightText>
+
+export const Default: Story = {
+  decorators: [
+    (Story) => (
+      <div className="bg-gray-200">
+        <Story />
+      </div>
+    ),
+  ],
+  args: {
+    text: '“우리는 애자일(Agile)한 조직으로 변모하기 위해 노력해왔습니다. 1년 동안 모두가 힘써주신 덕분에 높은 성과를 낼 수 있었어요.”',
+    target: '애자일(Agile)',
+  },
+}

5a6ed4bc1252611ba6d14de40ac426b1fcd26d42
lsy20140 <lsy20140@naver.com>
2024-08-21 15:40:43 +0900

    design: 별별 저장소 페이지 UI 추가

--- src/app/bookmarks/page.tsx
+++ src/app/bookmarks/page.tsx
@@ -6,17 +6,20 @@ import TopButton from '@/components/shared/TopButton'
 import { FilterType, SimpleWordType } from '@/types/word'
 import { useSearchParams } from 'next/navigation'
 import WordListItem from '@/components/shared/WordListItem'
+import Link from 'next/link'
+import Image from 'next/image'
 
 export default function BookmarksPage() {
   const searchParams = useSearchParams()
   const filters: FilterType[] = ['전체', '개발', '디자인', '비즈니스']
   const category: any = searchParams.get('category') ?? '전체'
+  const wordCount = words.length
 
   return (
     <>
       <div className="relative overflow-y-auto bg-background text-onSurface-300">
         <Header title="별별 저장소" />
-        <div className="flex gap-2 px-4 mt-[92px]">
+        <div className="flex gap-2 px-4 mt-[92px] mb-5">
           {filters.map((filter: FilterType, idx: number) => (
             <TabFilter
               isSelected={filter === category}
@@ -25,6 +28,27 @@ export default function BookmarksPage() {
             />
           ))}
         </div>
+        <div className="px-4 mb-2">
+          <p className="text-body3 text-onSurface-200 mb-4">
+            나만의 별 단어 {wordCount}
+          </p>
+          <Link
+            href={'/dictionary'}
+            className="flex justify-between p-4 rounded-lg bg-gray-800 hover:bg-gray-700"
+          >
+            <p className="text-sub3">
+              <span className="text-primary-400">별 단어</span>라고 생각이 드는
+              실무 용어를 더 추가해보세요.
+            </p>
+            <Image
+              alt="dictionary"
+              width={20}
+              height={20}
+              src={'/icons/right_arrow.svg'}
+            />
+          </Link>
+        </div>
+
         {words.map((word: SimpleWordType, idx: number) => (
           <WordListItem key={word.id} word={word} isMarked showBookmarkBtn />
         ))}

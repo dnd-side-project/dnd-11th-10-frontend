259659dd3ee5f0f33aa53d949200e9c1c001f360
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-23 13:10:33 +0900

    chore: splash data text 하나로 합치기

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,25 +4,21 @@ import Button from '@/components/common/Button'
 import { loginWithGoogle } from '@/api/auth/[...nextauth]/route'
 
 interface SplashData {
-  text1: string
-  text2: string
+  text: string
   img: string
 }
 
 const splashData: SplashData[] = [
   {
-    text1: '실무 용어를 검색하고',
-    text2: '뜻과 예문을 알아보아요',
+    text: '실무 용어를 검색하고\n뜻과 예문을 알아보아요',
     img: '/images/splash_01.svg',
   },
   {
-    text1: '업무에서 일어나는 일을',
-    text2: '댓글로 남기고 소통해요',
+    text: '업무에서 일어나는 일을\n댓글로 남기고 소통해요',
     img: '/images/splash_02.svg',
   },
   {
-    text1: '퀴즈를 풀어서 개발, 디자인,',
-    text2: '비지니스 용어를 학습할 수 있어요',
+    text: '퀴즈를 풀어서 개발, 디자인,\n비지니스 용어를 학습할 수 있어요',
     img: '/images/splash_03.svg',
   },
 ]

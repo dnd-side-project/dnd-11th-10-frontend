bc70f91b2647c51ea462eb812c570eb7f1b81975
lsy20140 <lsy20140@naver.com>
2024-08-23 17:32:02 +0900

    feat: Input 공통 컴포넌트 추가

---
+++ src/components/common/Input/index.tsx
@@ -0,0 +1,21 @@
+import { cn } from '@/lib/core'
+import { InputHTMLAttributes, forwardRef } from 'react'
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
+
+export default forwardRef<HTMLInputElement, InputProps>(function Input(
+  { className, ...props },
+  ref,
+) {
+  return (
+    <input
+      ref={ref}
+      {...props}
+      className={cn(
+        'resize-none w-full flex items-center justify-center py-4 px-4 rounded-lg outline outline-[0.5px] outline-primary-400 bg-gray-700 focus:outline-[1.5px] ',
+        className,
+      )}
+      onChange={props.onChange}
+    />
+  )
+})

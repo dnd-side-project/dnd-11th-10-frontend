8d916c6bc9eb4cebf541d7e541cf3636066ca806
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-27 17:19:39 +0900

    chore: interface 수정

--- src/components/shared/QuizResultCard/index.tsx
+++ src/components/shared/QuizResultCard/index.tsx
@@ -1,5 +1,13 @@
+import { ExplanationInfo } from '@/types/quizresult'
 import React from 'react'
 
-export default function QuizReultCard() {
+export default function QuizReultCard({
+  wordId,
+  isCorrect,
+  isMarked,
+  name,
+  selectedOptionDescription,
+  answerOptionDescription,
+}: ExplanationInfo) {
   return <div>QuizReultCard</div>
 }
--- src/types/quizresult.ts
+++ src/types/quizresult.ts
@@ -1,8 +1,14 @@
-export interface QuizResult {
+export interface ExplanationInfo {
   wordId: number
   isCorrect: boolean
   isMarked: boolean
   name: string
   selectedOptionDescription: string
   answerOptionDescription: string
+}
+
+export interface QuizResult {
+  id: number
+  correctCount: number
+  explanationInfo: ExplanationInfo[]
 }

48eeb9faf9c4e550b0c5460f9b03f150f7116cae
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-27 20:54:38 +0900

    chore: 간격 및 텍스트 굵기 수정

--- src/components/shared/QuizResultCard/index.tsx
+++ src/components/shared/QuizResultCard/index.tsx
@@ -12,11 +12,8 @@ export default function QuizResultCard({
   answerOptionDescription,
 }: ExplanationInfo) {
   return (
-    <div
-      className="p-6 w-full bg-gray-800 text-onSurface-200 rounded-xl"
-      key={name}
-    >
-      <div className="flex justify-between">
+    <div className="px-3 py-6 w-full bg-gray-800  rounded-xl" key={name}>
+      <div className="mb-3 flex justify-between">
         <div className="flex">
           <Image
             src={isCorrect ? '/icons/correct.svg' : '/icons/wrong.svg'}
@@ -24,16 +21,16 @@ export default function QuizResultCard({
             width={20}
             height={20}
           />
-          <p className="ml-2">{name}</p>
+          <p className="ml-2 text-onSurface-300 font-medium">{name}</p>
         </div>
         <BookmarkButton wordId={wordId} isMarked={isMarked} />
       </div>
       {isCorrect && (
-        <div className="line-through text-[14px] text-onSurface-100">
+        <div className="line-through text-[14px] text-onSurface-100 font-normal">
           <p>{selectedOptionDescription}</p>
         </div>
       )}
-      <div className="flex break-keep text-[18px]">
+      <div className="flex break-keep text-[18px] text-onSurface-200">
         정답 :&nbsp;<p>{answerOptionDescription}</p>
       </div>
     </div>

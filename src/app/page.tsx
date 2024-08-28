95e339c552de3ede2bd6df9710956c9529869932
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-28 14:33:06 +0900

    refactor: 구글로그인 버튼 사용 및 splash data 분리

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,11 @@
 'use client'
-import Image from 'next/image'
 import Button from '@/components/common/Button'
-import { loginWithGoogle } from '@/api/auth/[...nextauth]/route'
-
-interface SplashData {
-  text: string
-  img: string
-}
-
-const splashData: SplashData[] = [
-  {
-    text: '실무 용어를 검색하고\n뜻과 예문을 알아보아요',
-    img: '/images/splash_01.svg',
-  },
-  {
-    text: '업무에서 일어나는 일을\n댓글로 남기고 소통해요',
-    img: '/images/splash_02.svg',
-  },
-  {
-    text: '퀴즈를 풀어서 개발, 디자인,\n비지니스 용어를 학습할 수 있어요',
-    img: '/images/splash_03.svg',
-  },
-]
+import GoogleLoginButton from '@/components/shared/GoogleLoginButton'
 
 export default function Home() {
   return (
     <div className="">
-      <Button
-        type={'white'}
-        isFullWidth={true}
-        onClick={() => loginWithGoogle()}
-      >
-        <div className="flex justify-center">
-          <Image
-            src={'/icons/google.svg'}
-            alt={'google.svg'}
-            width={23}
-            height={23}
-            className="mr-3"
-          />
-          <p>Google로 시작하기</p>
-        </div>
-      </Button>
+      <GoogleLoginButton />
       <div className="mt-[10px]">
         <Button type={'black'} isFullWidth={true}>
           비회원으로 시작하기
--- src/components/shared/GoogleLoginButton/index.tsx
+++ src/components/shared/GoogleLoginButton/index.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function GoogleLoginButton() {
   return (
-    <button className="w-full py-4 px-6 rounded-lg flex justify-center bg-white">
+    <button className="w-full py-4 px-6 rounded-lg flex justify-center bg-white hover:bg-onSurface-300 cursor-pointer">
       <div className="flex gap-3">
         <Image alt="google" src={'/icons/google.svg'} width={20} height={20} />
         <p className="font-medium text-black">Google로 시작하기</p>

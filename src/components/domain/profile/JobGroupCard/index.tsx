a2fa111243eba0b3f427668d5411cb82bd784899
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-30 03:26:17 +0900

    feat: 조건부 스타일링 추가 및 상태관리

--- src/components/domain/profile/JobGroupCard/index.tsx
+++ src/components/domain/profile/JobGroupCard/index.tsx
@@ -1,10 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import { JobGroup } from '@/types/profile'
 
 export default function JobGroupCard({ id, name }: JobGroup) {
+  const [selected, setSelected] = useState(false)
+
+  const handleClick = () => {
+    setSelected(!selected)
+  }
+
   return (
-    <div className="flex p-4 bg-gray-800 rounded-2xl text-onSurface-300">
+    <div
+      className={`flex p-4 bg-gray-800 rounded-2xl text-onSurface-300 ${
+        selected &&
+        'bg-primary-0 text-background border-solid border-[1.5px] border-primary-400'
+      }`}
+      onClick={handleClick}
+    >
       <Image
         src={`/icons/job_group_0${id}.svg`}
         alt={`job_group_${id}.svg`}

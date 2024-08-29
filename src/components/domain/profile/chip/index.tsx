192a74e2baaf9c83b72c9358eee0fe02a1ece16c
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-08-29 17:02:00 +0900

    feat: 클릭했을 때 활성화

--- src/components/domain/profile/chip/index.tsx
+++ src/components/domain/profile/chip/index.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ProfileChip } from '@/types/profileChip'
 
 export default function Chip({ id, name }: ProfileChip) {
+  const [selected, setSelected] = useState(false)
+
+  const handleClick = () => {
+    setSelected(!selected)
+  }
+
   return (
     <div
-      className="w-fit px-5 py-2 border-solid border-[1.5px] border-outline text-sub3 text-onSurface-300 rounded-[0.25rem] hover:cursor-pointer"
+      className={`w-fit px-5 py-2 rounded-[0.25rem] hover:cursor-pointer ${
+        selected
+          ? 'bg-primary-400 text-background'
+          : 'border-solid border-[1.5px] border-outline text-onSurface-300'
+      }`}
       key={id}
+      onClick={handleClick}
     >
       {name}
     </div>
--- src/types/profileChip.ts
+++ src/types/profileChip.ts
@@ -1,5 +1,4 @@
 export interface ProfileChip {
   id: number
-  type: '기업' | '경력'
   name: string
 }

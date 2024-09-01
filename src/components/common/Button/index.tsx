5f105a155a75178ce2dc1269ad4f713645cffd8e
lsy20140 <lsy20140@naver.com>
2024-09-01 18:14:15 +0900

    design: Button 컴포넌트 타입 gradient인 경우 transition 제거 -> opacity 0.9로 변경

--- src/components/common/Button/index.tsx
+++ src/components/common/Button/index.tsx
@@ -22,7 +22,8 @@ export default function Button({
         `py-4 px-6 text-onSurface-300 rounded-lg w-fit font-medium leading-6`,
         {
           'bg-gray-800 hover:bg-gray-700': type === 'default',
-          'btn-gradient text-background': type === 'gradient',
+          'bg-btn-gradient hover:bg-btn-gradient-hover text-background':
+            type === 'gradient',
           'bg-background hover:bg-gray-800': type === 'black',
           'bg-gray-800 text-onSurface-100': type === 'disabled',
           'bg-gray-600 hover:bg-gray-500': type === 'light',

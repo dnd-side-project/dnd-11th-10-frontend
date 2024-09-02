44c4d9f27279ee66ccc39f1c798b9199d2fb1105
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-09-02 21:17:16 +0900

    refactor: splash data 분리 및 비회원 라우터 이동

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,33 +5,12 @@ import Button from '@/components/common/Button'
 import GoogleLoginButton from '@/components/shared/GoogleLoginButton'
 import Image from 'next/image'
 import Link from 'next/link'
-
-interface SplashData {
-  id?: number
-  text: string
-  img: string
-}
-
-const splashData: SplashData[] = [
-  {
-    id: 1,
-    text: '실무 용어를 검색하고\n뜻과 예문을 알아보아요',
-    img: '/images/splash_01.svg',
-  },
-  {
-    id: 2,
-    text: '업무에서 일어나는 일을\n댓글로 남기고 소통해요',
-    img: '/images/splash_02.svg',
-  },
-  {
-    id: 3,
-    text: '퀴즈를 풀어서 개발, 디자인,\n비지니스 용어를 학습할 수 있어요',
-    img: '/images/splash_03.svg',
-  },
-]
+import { splashData } from '@/constants/splashData'
+import { useRouter } from 'next/navigation'
 
 export default function Home() {
   const [currentSlide, setCurrentSlide] = useState(0)
+  const router = useRouter()
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -85,7 +64,11 @@ export default function Home() {
           <GoogleLoginButton />
         </Link>
         <div className="mt-[10px]">
-          <Button type={'black'} isFullWidth={true}>
+          <Button
+            type={'black'}
+            isFullWidth={true}
+            onClick={() => router.push('/home/dictionary')}
+          >
             비회원으로 시작하기
           </Button>
         </div>

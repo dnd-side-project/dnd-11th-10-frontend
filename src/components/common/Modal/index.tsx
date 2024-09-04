e582d0cf89ade34443f628aba11c11a976948fda
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-09-05 03:46:40 +0900

    feat: 퀴즈 중간에 나갈경우 나타나는 Modal 추가

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -1,5 +1,13 @@
-import React from 'react'
+type ModalType = {
+  children: React.ReactNode
+}
 
-export default function Modal() {
-  return <div>Modal</div>
+export default function Modal({ children }: ModalType) {
+  return (
+    <div>
+      <div className="py-5 px-4 bg-gray-700 text-onSurface-300 rounded-2xl">
+        {children}
+      </div>
+    </div>
+  )
 }
--- src/components/shared/QuizProblemStopModal/index.tsx
+++ src/components/shared/QuizProblemStopModal/index.tsx
@@ -1,5 +1,36 @@
-import React from 'react'
+import Button from '@/components/common/Button'
+import Modal from '@/components/common/Modal'
 
-export default function QuizProblemStopModal() {
-  return <div>QuizProblemStopModal</div>
+interface QuizProblemStopModalProps {
+  isModalOpen: boolean
+  totalProblem: number
+  currentProblem: number
+}
+
+export default function QuizProblemStopModal({
+  isModalOpen,
+  totalProblem,
+  currentProblem,
+}: QuizProblemStopModalProps) {
+  if (!isModalOpen) return null
+
+  return (
+    <Modal>
+      <div className="flex flex-col text-center">
+        <p className="text-h2">퀴즈를 그만두시겠어요?</p>
+        <div className="flex m-auto text-h2">
+          <p className="text-primary-400">{totalProblem - currentProblem}</p>
+          <p>문제밖에 남지 않았어요!</p>
+        </div>
+        <p className="text-onSurface-200 text-body2">
+          지금 그만두면 다시 처음부터 진행해야해요.
+        </p>
+      </div>
+
+      <div className="flex justify-between">
+        <Button type={'light'}>계속할게요</Button>
+        <Button type={'gradient'}>그만할게요</Button>
+      </div>
+    </Modal>
+  )
 }

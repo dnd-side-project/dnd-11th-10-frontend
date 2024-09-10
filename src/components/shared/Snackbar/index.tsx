07568050a6d8302613a52c2680757853e7a07c45
lsy20140 <lsy20140@naver.com>
2024-09-10 22:56:35 +0900

    design: Snackbar 컴포넌트 css 수정

--- src/components/shared/Snackbar/index.tsx
+++ src/components/shared/Snackbar/index.tsx
@@ -18,9 +18,13 @@ export default function Snackbar() {
   if (!snackbarType) return null
   return (
     <div className="fixed bottom-10 left-1/2 transform -translate-x-1/2 w-full max-w-[430px]">
-      <div className="flex justify-between bg-gray-600 text-sub2 text-center py-4 px-7 rounded-lg mx-4 animate-fade-in">
-        <p>{SNACKBAR[snackbarType].text}</p>
-        <p>{SNACKBAR[snackbarType].actionText}</p>
+      <div className="flex justify-between items-center bg-gray-600 text-center py-4 px-7 rounded-lg mx-4 animate-fade-in">
+        <p className="text-onSurface-300 text-sub2">
+          {SNACKBAR[snackbarType].text}
+        </p>
+        <p className="text-sub3 text-primary-400">
+          {SNACKBAR[snackbarType].actionText}
+        </p>
       </div>
     </div>
   )

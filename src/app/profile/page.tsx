167904bb8e7185b95fc3196483c944ea3aa6afee
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-09-13 21:14:33 +0900

    feat: 기업 레이아웃 추가

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,11 +1,21 @@
 'use client'
 
 import Button from '@/components/common/Button'
+import Chip from '@/components/domain/profile/Chip'
 import JobGroupCard from '@/components/domain/profile/JobGroupCard'
 import { useState } from 'react'
 
 function Profile() {
+  const [step, setStep] = useState<'직군' | '기업' | '경력'>('직군')
   const jobGroupData = ['개발자', '디자이너', '기타']
+  const companyData = [
+    '비공개',
+    '대기업',
+    '중견기업',
+    '중소기업',
+    '스타트업',
+    '외국계',
+  ]
 
   const [isCategorySelect, setIsCategorySelect] = useState<boolean[]>(
     Array(jobGroupData.length).fill(false),
@@ -27,30 +37,58 @@ function Profile() {
 
   return (
     <div className="px-4 flex flex-col gap-[351] justify-between h-full text-slate-100">
-      <div className="mt-[90px] flex flex-col gap-[72px]">
-        <p className="text-h1 text-onSurface-300">
-          반가워요! <br />
-          어떤 직군이신가요?
-        </p>
-        <div className="mt-4 flex flex-col gap-4">
-          {jobGroupData.map((job, idx) => (
-            <JobGroupCard
-              key={idx}
-              id={idx + 1}
-              name={job}
-              selected={isCategorySelect[idx]}
-              onClick={() => handleClick(idx)}
-            />
-          ))}
-        </div>
-      </div>
-      <Button
-        isFullWidth
-        type={isAnyCategorySelected ? 'gradient' : 'default'}
-        disabled={!isAnyCategorySelected}
-      >
-        다음
-      </Button>
+      {step === '직군' && (
+        <>
+          <div className="mt-[90px] flex flex-col gap-[72px]">
+            <p className="text-h1 text-onSurface-300">
+              반가워요! <br />
+              어떤 직군이신가요?
+            </p>
+            <div className="mt-4 flex flex-col gap-4">
+              {jobGroupData.map((job, idx) => (
+                <JobGroupCard
+                  key={idx}
+                  id={idx + 1}
+                  name={job}
+                  selected={isCategorySelect[idx]}
+                  onClick={() => handleClick(idx)}
+                />
+              ))}
+            </div>
+          </div>
+          <Button
+            isFullWidth
+            type={isAnyCategorySelected ? 'gradient' : 'default'}
+            onClick={() => setStep('기업')}
+            disabled={!isAnyCategorySelected}
+          >
+            다음
+          </Button>
+        </>
+      )}
+      {step === '기업' && (
+        <>
+          <div className="mt-[90px] flex flex-col gap-10">
+            <p className="text-h1 text-onSurface-300">
+              어느 기업에서 근무 중이신가요?
+            </p>
+            <div className="mt-4 flex flex-wrap gap-4">
+              {companyData.map((company, idx) => (
+                <Chip id={idx} name={company} key={idx} />
+              ))}
+            </div>
+          </div>
+
+          <Button
+            isFullWidth
+            type={isAnyCategorySelected ? 'gradient' : 'default'}
+            onClick={() => setStep('경력')}
+            disabled={!isAnyCategorySelected}
+          >
+            다음
+          </Button>
+        </>
+      )}
     </div>
   )
 }
--- src/components/domain/profile/chip/index.tsx
+++ src/components/domain/profile/chip/index.tsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 import { ProfileChip } from '@/types/profileChip'
 
-export default function Chip({ id, type, name }: ProfileChip) {
+function Chip({ id, type, name }: ProfileChip) {
   const [selected, setSelected] = useState(false)
 
   const handleClick = () => {
@@ -11,7 +11,7 @@ export default function Chip({ id, type, name }: ProfileChip) {
 
   return (
     <div
-      className={`w-fit px-5 py-2 rounded-[0.25rem] hover:cursor-pointer ${
+      className={`px-5 py-2 rounded-[0.25rem] hover:cursor-pointer ${
         selected
           ? 'bg-primary-400 text-background'
           : 'border-solid border-[1.5px] border-outline text-onSurface-300'
@@ -23,3 +23,4 @@ export default function Chip({ id, type, name }: ProfileChip) {
     </div>
   )
 }
+export default Chip

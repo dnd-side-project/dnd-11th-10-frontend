80a7e257f9f66fb0dcb4636504c299ddc16499f4
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-09-17 23:47:18 +0900

    chore: 선언적 모달로 변경

--- src/components/common/Modal/index.tsx
+++ src/components/common/Modal/index.tsx
@@ -5,6 +5,7 @@ type ModalType = {
 export default function Modal({ children }: ModalType) {
   return (
     <div>
+      <div className="fixed bg-#0E121B opacity-50" />
       <div className="py-5 px-4 bg-gray-700 text-onSurface-300 rounded-2xl">
         {children}
       </div>

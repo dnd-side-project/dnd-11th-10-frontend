be2087f578af34172be2d4139412b51c92627ef0
lsy20140 <lsy20140@naver.com>
2024-09-19 21:42:40 +0900

    feat: 반응이 뜨거운 댓글 목록 조회 api 연동 및 prefetch 방식 적용

--- src/app/home/dictionary/page.tsx
+++ src/app/home/dictionary/page.tsx
@@ -1,17 +1,32 @@
+import { getPopularComments } from '@/api/comment'
 import Bookmarks from '@/components/domain/home/dictionary/Bookmarks'
 import PopularCommentsList from '@/components/domain/home/dictionary/PopularCommentsList'
 import { PopularComments } from '@/components/domain/home/dictionary/PopularCommentsList/data'
 import RecentlyAddedWords from '@/components/domain/home/dictionary/RecentlyAddedWords'
 import { WordsList } from '@/components/domain/home/dictionary/RecentlyAddedWords/data'
 import ViewAllWords from '@/components/domain/home/dictionary/ViewAllWords'
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from '@tanstack/react-query'
 
-export default function DictionaryTab() {
+export default async function DictionaryTab() {
+  const queryClient = new QueryClient()
+
+  await queryClient.prefetchQuery({
+    queryKey: ['comments', 'admin'],
+    queryFn: getPopularComments,
+  })
+
   return (
     <div className="w-full my-12 flex flex-col gap-[60px]">
-      <ViewAllWords />
-      <RecentlyAddedWords wordsList={WordsList} />
-      <Bookmarks />
-      <PopularCommentsList PopularComments={PopularComments} />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <ViewAllWords />
+        <RecentlyAddedWords wordsList={WordsList} />
+        <Bookmarks />
+        <PopularCommentsList />
+      </HydrationBoundary>
     </div>
   )
 }

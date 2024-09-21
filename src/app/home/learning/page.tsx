9e6295eb0d56788c08eedf216961560e53b2b6a7
lsy20140 <lsy20140@naver.com>
2024-09-22 00:34:36 +0900

    feat: 오늘의 용어 조회 api 연동 및 server component prefetch 적용

--- src/app/home/learning/page.tsx
+++ src/app/home/learning/page.tsx
@@ -1,14 +1,31 @@
+import { getTodayWords } from '@/api/words'
 import { WordsList } from '@/components/domain/home/dictionary/RecentlyAddedWords/data'
 import CommunicationStats from '@/components/domain/home/learning/CommunicationStats'
 import TodayQuiz from '@/components/domain/home/learning/TodayQuiz'
 import TodayWords from '@/components/domain/home/learning/TodayWords'
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from '@tanstack/react-query'
 
-export default function LearningTab() {
+export default async function LearningTab() {
+  const queryClient = new QueryClient()
+
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: ['words', 'today'],
+      queryFn: getTodayWords,
+    }),
+  ])
+
   return (
     <div className="w-full my-12 flex flex-col items-center gap-[60px]">
-      <TodayQuiz todaySolvedCnt={50} />
-      <TodayWords wordsList={WordsList} />
-      <CommunicationStats />
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <TodayQuiz todaySolvedCnt={50} />
+        <TodayWords />
+        <CommunicationStats />
+      </HydrationBoundary>
     </div>
   )
 }

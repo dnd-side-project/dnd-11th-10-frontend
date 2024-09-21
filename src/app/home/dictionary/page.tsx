c4ecaa670e484a5db3eb43acfc51e1da5090ddfe
lsy20140 <lsy20140@naver.com>
2024-09-22 00:05:21 +0900

    feat: 최근 등록된 용어 조회 api 연동 및 custom hook 생성

--- src/app/home/dictionary/page.tsx
+++ src/app/home/dictionary/page.tsx
@@ -1,9 +1,8 @@
 import { getPopularComments } from '@/api/comment'
+import { getRecentWords } from '@/api/words'
 import Bookmarks from '@/components/domain/home/dictionary/Bookmarks'
 import PopularCommentsList from '@/components/domain/home/dictionary/PopularCommentsList'
-import { PopularComments } from '@/components/domain/home/dictionary/PopularCommentsList/data'
 import RecentlyAddedWords from '@/components/domain/home/dictionary/RecentlyAddedWords'
-import { WordsList } from '@/components/domain/home/dictionary/RecentlyAddedWords/data'
 import ViewAllWords from '@/components/domain/home/dictionary/ViewAllWords'
 import {
   HydrationBoundary,
@@ -14,16 +13,22 @@ import {
 export default async function DictionaryTab() {
   const queryClient = new QueryClient()
 
-  await queryClient.prefetchQuery({
-    queryKey: ['comments', 'admin'],
-    queryFn: getPopularComments,
-  })
+  await Promise.all([
+    queryClient.prefetchQuery({
+      queryKey: ['words', 'recent'],
+      queryFn: getRecentWords,
+    }),
+    queryClient.prefetchQuery({
+      queryKey: ['comments', 'popular'],
+      queryFn: getPopularComments,
+    }),
+  ])
 
   return (
     <div className="w-full my-12 flex flex-col gap-[60px]">
       <HydrationBoundary state={dehydrate(queryClient)}>
         <ViewAllWords />
-        <RecentlyAddedWords wordsList={WordsList} />
+        <RecentlyAddedWords />
         <Bookmarks />
         <PopularCommentsList />
       </HydrationBoundary>
--- src/components/domain/home/dictionary/RecentlyAddedWords/index.tsx
+++ src/components/domain/home/dictionary/RecentlyAddedWords/index.tsx
@@ -1,15 +1,15 @@
+'use client'
 import HorizontalScrollArea from '@/components/common/HorizontalScrollArea'
-import WordCard, { WordCardProps } from '@/components/shared/WordCard'
+import WordCard from '@/components/shared/WordCard'
+import { useGetRecentWords } from '@/hooks/word/useGetRecentWords'
 
-export default function RecentlyAddedWords({
-  wordsList,
-}: {
-  wordsList: WordCardProps[]
-}) {
+export default function RecentlyAddedWords() {
+  const { words } = useGetRecentWords()
+  if (typeof words === 'string' || !words) return
   return (
     <>
       <HorizontalScrollArea title="최근 등록된 용어 🌟" scrollDivisor={2}>
-        {wordsList.map(({ id, name, meaning, category }, idx) => (
+        {words.map(({ id, name, meaning, category }, idx) => (
           <WordCard
             key={id}
             id={id}

513b94ba4713f72e40d6204bfcd8dc1e5b9d7136
lsy20140 <lsy20140@naver.com>
2024-09-22 15:22:50 +0900

    feat: admin 용어 목록 조회 api 연동

--- src/api/admin/words/index.ts
+++ src/api/admin/words/index.ts
@@ -10,3 +10,8 @@ export async function addNewWord(word: AddWordType) {
   const res = await post('/admin/words', word)
   return res
 }
+
+export async function getAllWords() {
+  const res = await get<AdminWordType[]>(`/admin/allwords`)
+  return res
+}

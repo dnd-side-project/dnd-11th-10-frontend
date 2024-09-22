c3b15b9f3b7c8636e741b74a81ddd7de0c3beb6b
lsy20140 <lsy20140@naver.com>
2024-09-22 17:13:32 +0900

    style: admin api 주석 추가

--- src/api/admin/words/index.ts
+++ src/api/admin/words/index.ts
@@ -1,16 +1,19 @@
 import { get, post, put } from '@/lib/axios'
 import { AddWordType, AdminWordType } from '@/types/word'
 
+// 용어 id로 용어 상세 정보 조회
 export async function getWordDetailById(wordId: number) {
   const res = await get<AdminWordType>(`/admin/words/${wordId}`)
   return res
 }
 
+// 용어 등록
 export async function addNewWord(word: AddWordType) {
   const res = await post('/admin/words', word)
   return res
 }
 
+// 전체 용어 목록 조회
 export async function getAllWords() {
   const res = await get<AdminWordType[]>(`/admin/allwords`)
   return res

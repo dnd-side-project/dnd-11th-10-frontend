ed2794aaefb78180c9bebb32d7ae6f60b2659172
lsy20140 <lsy20140@naver.com>
2024-09-22 18:01:41 +0900

    feat: admin 용어 삭제 버튼 컴포넌트 분리 및 적용

--- src/app/admin/words/[slug]/page.tsx
+++ src/app/admin/words/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { getWordDetailById } from '@/api/admin/words'
+import RemoveWordButton from '@/components/domain/admin/DeleteWordButton'
 import AdminWordForm from '@/components/domain/admin/WordForm'
 
 export default async function WordDetailPage({
@@ -8,9 +9,13 @@ export default async function WordDetailPage({
 }) {
   const wordId = parseInt(params.slug.split('/').at(-1) ?? '')
   const res = await getWordDetailById(wordId)
+
   return (
     <div className="mx-72 my-8 max-lg:mx-4">
-      <p className="text-h2 mb-6">용어 상세</p>
+      <div className="flex justify-between items-center mb-6">
+        <p className="text-h2">용어 상세</p>
+        <RemoveWordButton wordId={wordId} />
+      </div>
       <AdminWordForm initWord={res} />
     </div>
   )

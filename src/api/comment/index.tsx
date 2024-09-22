006e61b6043fe44870c87f60b1dd916b33bbbdb5
lsy20140 <lsy20140@naver.com>
2024-09-22 22:24:20 +0900

    feat: 댓글 목록 조회 api 연동

--- src/api/comment/index.tsx
+++ src/api/comment/index.tsx
@@ -1,9 +1,17 @@
 import { get } from '@/lib/axios'
-import { SimpleCommentType } from '@/types/comment'
+import { DetailCommentType, SimpleCommentType } from '@/types/comment'
 import { SuccessResponse } from '@/types/response'
 
 export async function getPopularComments() {
   const res =
     await get<SuccessResponse<SimpleCommentType[]>>('/comments/popular')
   return res.comments
+}
+
+// 용어 상세 페이지 댓글 목록 조회
+export async function getCommentsByWordId(wordId: number) {
+  const res = await get<SuccessResponse<DetailCommentType[]>>(
+    `/words/${wordId}/comments`,
+  )
+  return res.comments as DetailCommentType[]
 }

a1e81160ae939dbcd8ff0073786911b4d8dcdc30
lsy20140 <lsy20140@naver.com>
2024-09-22 17:15:26 +0900

    design: admin 용어 수정, 등록 페이지 margin 수정

--- src/app/admin/words/[slug]/page.tsx
+++ src/app/admin/words/[slug]/page.tsx
@@ -9,7 +9,7 @@ export default async function WordDetailPage({
   const wordId = parseInt(params.slug.split('/').at(-1) ?? '')
   const res = await getWordDetailById(wordId)
   return (
-    <div className="mx-72 mt-8 max-lg:mx-4">
+    <div className="mx-72 my-8 max-lg:mx-4">
       <p className="text-h2 mb-6">용어 상세</p>
       <AdminWordForm initWord={res} />
     </div>
--- src/app/admin/words/add/page.tsx
+++ src/app/admin/words/add/page.tsx
@@ -3,7 +3,7 @@ import AdminWordForm from '@/components/domain/admin/WordForm'
 export default function AddWordPage() {
   return (
     <>
-      <div className="mx-72 mt-8 max-lg:mx-4">
+      <div className="mx-72 my-8 max-lg:mx-4">
         <p className="text-h2 mb-6">용어 등록</p>
         <AdminWordForm />
       </div>

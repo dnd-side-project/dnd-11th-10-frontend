b082a7f0ab0e3b8b21da281775fef07309ed9b4f
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-09-25 21:40:58 +0900

    feat: Information component 추가

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,7 +1,77 @@
 import React from 'react'
+import Button from '@/components/common/Button'
+import Image from 'next/image'
+
+interface Account {
+  nickname: string
+  profileImage: string
+  careerInfo: {
+    jobGroup: string
+    company: string
+    experience: string
+  }
+  quizInfo: {
+    totalCategoryQuizCount: number
+    designQuizCount: number
+    businessQuizCount: number
+    developQuizCount: number
+  }
+}
+
+const accountData: Account = {
+  nickname: '상큼한화성009',
+  profileImage: '/images/profile.svg',
+  careerInfo: {
+    jobGroup: '개발자',
+    company: '비공개',
+    experience: '1년 차 미만',
+  },
+  quizInfo: {
+    totalCategoryQuizCount: 0,
+    designQuizCount: 0,
+    businessQuizCount: 0,
+    developQuizCount: 0,
+  },
+}
 
 function MyPage() {
-  return <div className='text-primary-300'>MyPage</div>
+  return (
+    <div className="px-4">
+      <div className="bg-gray-800 px-4 py-6 rounded-2xl">
+        <div className="flex">
+          <Image
+            src={accountData.profileImage}
+            alt="profile.svg"
+            width={40}
+            height={40}
+          />
+          <div>
+            <p className="text-body2 text-onSurface-300">
+              {accountData.nickname}
+            </p>
+            <p className="flex text-onSurface-200">
+              {accountData.careerInfo.jobGroup}
+              <span className=''>
+                <svg width="2" height="2" viewBox="0 0 2 2" fill="none">
+                  <circle cx="1" cy="1" r="1" fill="white" fill-opacity="0.6" />
+                </svg>
+              </span>
+              {accountData.careerInfo.company}
+              <span>
+                <svg width="2" height="2" viewBox="0 0 2 2" fill="none">
+                  <circle cx="1" cy="1" r="1" fill="white" fill-opacity="0.6" />
+                </svg>
+              </span>
+              {accountData.careerInfo.experience}
+            </p>
+          </div>
+        </div>
+        <Button isFullWidth type={'light'}>
+          프로필 편집하기
+        </Button>
+      </div>
+    </div>
+  )
 }
 
 export default MyPage

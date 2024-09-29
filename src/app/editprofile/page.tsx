c79b8e3d2d8b8371eb1b380c28089c727404ffb3
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-09-29 20:00:07 +0900

    feat: 페이지 연결

--- src/app/editprofile/page.tsx
+++ src/app/editprofile/page.tsx
@@ -1,5 +1,5 @@
 function EditPofile() {
-  return <div className="px-4 flex flex-col gap-10"></div>
+  return <div className="px-4 flex flex-col gap-10 ">프로필 수정</div>
 }
 
 export default EditPofile

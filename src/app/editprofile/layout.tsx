21266791c5c335afa9cd1cbff4dc57e30f64f80f
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-09-29 18:36:23 +0900

    feat: 프로필 수정 페이지 추가

---
+++ src/app/editprofile/layout.tsx
@@ -0,0 +1,14 @@
+import Header from '@/components/shared/Header'
+import React from 'react'
+
+type LayoutProps = {
+  children: React.ReactNode
+}
+export default function EditProfileLayout({ children }: LayoutProps) {
+  return (
+    <div className="pb-4 h-full">
+      <Header title={'프로필 수정'} />
+      <div className="h-[calc(100%-80px)]">{children}</div>
+    </div>
+  )
+}
---
+++ src/app/editprofile/page.tsx
@@ -0,0 +1,5 @@
+function EditPofile() {
+  return <div className="px-4 flex flex-col gap-10"></div>
+}
+
+export default EditPofile

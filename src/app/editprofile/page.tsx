4e07a3803671a76eb9ba13a3d44acdb242d63772
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-09-30 23:54:45 +0900

    feat: 프로필 수정 화면 구조 잡기

--- src/app/editprofile/page.tsx
+++ src/app/editprofile/page.tsx
@@ -1,5 +1,26 @@
+import Button from '@/components/common/Button'
+
 function EditPofile() {
-  return <div className="px-4 flex flex-col gap-10 ">프로필 수정</div>
+  return (
+    <div className="px-4 flex flex-col gap-10">
+      <div>
+        <p>이메일</p>
+      </div>
+      <div>
+        <p>닉네임</p>
+      </div>
+      <div>
+        <p>직군</p>
+      </div>
+      <div>
+        <p>기업</p>
+      </div>
+      <div>
+        <p>경력</p>
+      </div>
+      <Button type="gradient">수정 완료</Button>
+    </div>
+  )
 }
 
 export default EditPofile

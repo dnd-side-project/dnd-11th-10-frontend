b2dd6af3ef9028e130bb09bab9882d58ce239b4c
lsy20140 <lsy20140@naver.com>
2024-10-02 01:00:43 +0900

    feat: SkillAbilityType 추가 및 useGetAbility custom hook 생성

--- src/hooks/auth/useGetSkill.tsx
+++ src/hooks/auth/useGetSkill.tsx
@@ -1,11 +1,20 @@
-import { getUserPrecedence } from '@/api/auth/skill'
+import { getUserAbility, getUserPrecedence } from '@/api/auth/skill'
 import { useQuery } from '@tanstack/react-query'
 
-export default function useGetPrecedence() {
+export function useGetPrecedence() {
   const { data: precedence, isFetched } = useQuery({
     queryKey: ['skill', 'precedence'],
     queryFn: getUserPrecedence,
   })
 
   return { precedence, isFetched }
+}
+
+export function useGetAbility() {
+  const { data: ability, isFetched } = useQuery({
+    queryKey: ['skill', 'ability'],
+    queryFn: getUserAbility,
+  })
+
+  return { ability, isFetched }
 }

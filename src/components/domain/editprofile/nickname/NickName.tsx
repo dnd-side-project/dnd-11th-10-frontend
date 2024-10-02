8b690ad16fdfc88ad12aaf503945957af37eb7c9
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-10-03 03:12:16 +0900

    chore: input radius 추가

--- src/components/domain/editprofile/nickname/NickName.tsx
+++ src/components/domain/editprofile/nickname/NickName.tsx
@@ -5,7 +5,7 @@ function NickName() {
     <div className="flex flex-col gap-3">
       <p className="text-sub2 text-onSurface-200">닉네임</p>
       <input
-        className="p-4 w-full bg-gray-800 text-onSurface-300 outline-none"
+        className="p-4 w-full rounded-lg bg-gray-800 text-onSurface-300 outline-none"
         defaultValue={accountData.nickname}
         placeholder="닉네임을 입력하세요"
         maxLength={10}

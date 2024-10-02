65f167a304d20e9f9ff224ed41028091a249a628
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-10-02 15:09:20 +0900

    feat: userId 전역 상태 관리

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -2,12 +2,16 @@ import { create } from 'zustand'
 
 interface AuthState {
   accessToken: string | null
+  userId: number | null
+  setUserId: (id: number | null) => void
   setAccessToken: (token: string | null) => void
   clearAccessToken: () => void
 }
 
 export const useAuthStore = create<AuthState>((set) => ({
   accessToken: null,
+  userId: null,
+  setUserId: (id) => set({ userId: id }),
   setAccessToken: (token) => set({ accessToken: token }),
   clearAccessToken: () => set({ accessToken: null }),
 }))

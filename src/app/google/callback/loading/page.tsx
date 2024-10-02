4c873a5c749f0ce92eba11240c884552aab181cc
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-10-02 15:25:14 +0900

    feat: userId 가져오기

--- src/app/google/callback/loading/page.tsx
+++ src/app/google/callback/loading/page.tsx
@@ -8,14 +8,18 @@ const LoginCallback = () => {
   const router = useRouter()
   const searchParams = useSearchParams()
   const setAccessToken = useAuthStore((state) => state.setAccessToken)
+  const setUserId = useAuthStore((state) => state.setUserId)
   const [role, setRole] = useState(searchParams.get('role'))
 
   const accessToken = searchParams.get('accessToken')
   const isSignUp = searchParams.get('isSignUp')
+  const userId = searchParams.get('id')
+
   useEffect(() => {
     if (role && accessToken) {
       setAccessToken(accessToken)
       setRole(role)
+      setUserId(Number(userId))
     }
 
     if (accessToken) {
@@ -29,7 +33,16 @@ const LoginCallback = () => {
         router.push('/home/dictionary')
       }
     }
-  }, [role, accessToken, isSignUp, router, setAccessToken, setRole])
+  }, [
+    role,
+    accessToken,
+    isSignUp,
+    router,
+    setAccessToken,
+    setRole,
+    setUserId,
+    userId,
+  ])
 
   return <></>
 }

8c4aa00e8eab89b9edb800dcf599be4b72c53c49
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-10-03 02:59:31 +0900

    feat: 이미지 상태 관리

--- src/components/domain/editprofile/profileimg/ProfileImg.tsx
+++ src/components/domain/editprofile/profileimg/ProfileImg.tsx
@@ -14,19 +14,26 @@ function ProfileImg() {
     6: '/images/profile_06.svg',
     7: '/images/profile_07.svg',
     8: '/images/profile_08.svg',
-    9: '/images/profile_08.svg',
-    10: '/images/profile_09.svg',
+    9: '/images/profile_09.svg',
+    10: '/images/profile_10.svg',
   }
+
   const [isProfileImgOpen, setIsProfileImgOpen] = useState(false)
+  const [selectedImage, setSelectedImage] = useState(accountData.profileImage)
+
+  const handleImageSelect = (src: string) => {
+    setSelectedImage(src)
+  }
+
+  const handleSave = () => {
+    setIsProfileImgOpen(false)
+    console.log('Profile image saved:', selectedImage)
+  }
+
   return (
     <>
       <div className="relative m-auto">
-        <Image
-          src={accountData.profileImage}
-          width={100}
-          height={100}
-          alt="profile.svg"
-        />
+        <Image src={selectedImage} width={100} height={100} alt="profile.svg" />
         <div className="absolute bottom-0 right-0 p-2 w-[30px] h-[30px] bg-gray-800 rounded-full">
           <Image
             src={'icons/pen.svg'}
@@ -45,7 +52,7 @@ function ProfileImg() {
           <div className="flex flex-col gap-6">
             <div className="flex justify-center">
               <Image
-                src={accountData.profileImage}
+                src={selectedImage}
                 alt="profile.svg"
                 width={100}
                 height={100}
@@ -60,14 +67,12 @@ function ProfileImg() {
                     width={60}
                     height={60}
                     className="cursor-pointer"
-                    onClick={() =>
-                      console.log(`Profile image ${index + 1} selected`)
-                    }
+                    onClick={() => handleImageSelect(src)}
                   />
                 </div>
               ))}
             </div>
-            <Button isFullWidth type="gradient">
+            <Button isFullWidth type="gradient" onClick={handleSave}>
               저장
             </Button>
           </div>

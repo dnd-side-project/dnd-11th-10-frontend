6bdf7c23909f90b06d99fdcd0d31947194b59440
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-10-03 17:16:12 +0900

    feat: nickname 글자 수 상태 관리

--- src/components/domain/editprofile/email/Email.tsx
+++ src/components/domain/editprofile/email/Email.tsx
@@ -6,7 +6,7 @@ function Email() {
       <p className="text-sub2 text-onSurface-100">이메일</p>
       <input
         readOnly
-        className="p-4 w-full bg-gray-800 text-onSurface-100 outline-none"
+        className="p-4 w-full bg-gray-800 text-sub1 text-onSurface-100 outline-none"
         defaultValue={accountData.email}
       />
     </div>

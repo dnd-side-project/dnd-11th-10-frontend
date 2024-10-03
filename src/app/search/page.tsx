cc349fc2937fed32095eefa6d775ba674ee9ce44
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-10-03 17:07:36 +0900

    feat: 많이 찾아본 용어 title 추가

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,13 @@
 export default function SearchPage() {
   return (
-    <>
-      <p className="text-onSurface-300">검색페이지</p>
-    </>
+    <div className="px-4">
+      <div>
+        <p className="text-onSurface-300">많이 찾아본 용어</p>
+        <div>
+          <p>오후 00:00 업데이트</p>
+          <p>*12시간 간격 업데이트</p>
+        </div>
+      </div>
+    </div>
   )
 }

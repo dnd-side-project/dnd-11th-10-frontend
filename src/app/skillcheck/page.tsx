143786e5dd90d20f4c5052d5c3f05c8a87b98903
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-10-10 18:12:49 +0900

    feat: skillcheck 임시 추가

--- src/app/skillcheck/page.tsx
+++ src/app/skillcheck/page.tsx
@@ -1,7 +1,18 @@
+'use client'
+
+import Button from '@/components/common/Button'
+import { useRouter } from 'next/navigation'
 import React from 'react'
 
 function SkillCheck() {
-  return <div className="text-onSurface-300">SkillCheck</div>
+  const router = useRouter()
+  return (
+    <div className="text-onSurface-300">
+      <Button type="gradient" onClick={() => router.push('/home/dictionary')}>
+        능력 확인하러 가기
+      </Button>
+    </div>
+  )
 }
 
 export default SkillCheck

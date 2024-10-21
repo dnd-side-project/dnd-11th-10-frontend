e23473501245d88682745b080b4dbbb357ef9751
lsy20140 <lsy20140@naver.com>
2024-10-21 16:53:55 +0900

    refactor: 서버 요청인 경우 새 queryClient 생성하도록 queryClient 세팅 수정

---
+++ src/utils/getQueryClient.ts
@@ -0,0 +1,34 @@
+import {
+  QueryClient,
+  defaultShouldDehydrateQuery,
+  isServer,
+} from '@tanstack/react-query'
+
+function makeQueryClient() {
+  return new QueryClient({
+    defaultOptions: {
+      queries: {
+        retry: 1,
+        refetchOnMount: false,
+        refetchOnWindowFocus: false,
+        staleTime: 60 * 1000,
+      },
+      dehydrate: {
+        shouldDehydrateQuery: (query) =>
+          defaultShouldDehydrateQuery(query) ||
+          query.state.status === 'pending',
+      },
+    },
+  })
+}
+
+let browserQueryClient: QueryClient | undefined = undefined
+
+export function getQueryClient() {
+  if (isServer) {
+    return makeQueryClient()
+  } else {
+    if (!browserQueryClient) browserQueryClient = makeQueryClient()
+    return browserQueryClient
+  }
+}

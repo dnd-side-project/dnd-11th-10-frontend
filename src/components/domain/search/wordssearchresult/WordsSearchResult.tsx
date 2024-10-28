bbcc550545c8de235571c26e88da199ff421d4ca
Mijin Sim <80371353+azure-553@users.noreply.github.com>
2024-10-28 23:50:36 +0900

    feat: 검색된 용어 갯수 추가

--- src/components/domain/search/wordssearchresult/WordsSearchResult.tsx
+++ src/components/domain/search/wordssearchresult/WordsSearchResult.tsx
@@ -5,9 +5,11 @@ import { getSearchWords } from '@/api/search'
 import { SearchWord } from '@/types/searchWords'
 import EmptyLayout from '@/components/shared/EmptyLayout'
 import WordListItem from '@/components/shared/WordListItem'
+import Image from 'next/image'
 
 function WordsSearchResult({ keyword }: { keyword: string }) {
   const [words, setWords] = useState<SearchWord[]>([])
+  const wordsCount = 1
 
   useEffect(() => {
     const getWords = async () => {
@@ -29,6 +31,21 @@ function WordsSearchResult({ keyword }: { keyword: string }) {
         </div>
       ) : (
         <div className="text-onSurface-300">
+          <div className="px-4 flex justify-between">
+            <div className="flex gap-1 text-h2">
+              <p>검색된용어</p>
+              <p className="text-primary-400">{wordsCount}</p>
+            </div>
+            <p className="flex gap-1 p-1 text-body3">
+              정확도순
+              <Image
+                src="/icons/arrow_down.svg"
+                width={8}
+                height={8}
+                alt="arrow_down.svg"
+              />
+            </p>
+          </div>
           {words.map((word) => (
             <WordListItem
               word={{
